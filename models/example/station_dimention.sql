bd4751cc332e360c71f48f7ef10853e1a7d0c8bf
rmorampu@gmail.com <rmorampu@gmail.com>
2025-10-07 22:26:09 +0000

    station_dimention model created

---
+++ models/example/station_dimention.sql
@@ -0,0 +1,13 @@
+WITH BIKE AS (
+
+    select 
+    start_statio_id as station_id,
+    start_station_name,
+    start_lat,
+    start_lng
+    from {{ source('demo', 'bike') }}
+    where RIDE_ID != 'ride_id'
+
+)
+
+select * from Bike
